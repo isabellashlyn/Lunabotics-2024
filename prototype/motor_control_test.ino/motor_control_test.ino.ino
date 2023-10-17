d48e491d96ab113777b6ea75e3536bb46c345fea
hman68 <henrylepp3@gmail.com>
2023-10-16 19:13:46 -0700

    Created a test for the current sensor and updated readme.md

--- prototype/motor_control_test.ino/motor_control_test.ino.ino
+++ prototype/motor_control_test.ino/motor_control_test.ino.ino
@@ -1,9 +1,20 @@
+const int KILL_SWITCH = 1;
+
+bool ks;
 void setup() {
   // put your setup code here, to run once:
-
+  ks = false;
+  pinMode(KILL_SWITCH, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while (!ks) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.write((digitalRead(KILL_SWITCH) == HIGH) ? "1" : "0");
+    if(digitalRead(KILL_SWITCH) == HIGH){
+      ks = true;
+    }
+  }
+  digitalWrite(LED_BUILTIN, LOW);
 }
