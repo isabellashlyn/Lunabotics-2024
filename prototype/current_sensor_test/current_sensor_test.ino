5112db6ae668e8848d45557ff7d332267161a902
notpranavshinde <138583678+notpranavshinde@users.noreply.github.com>
2023-11-01 17:01:35 -0700

    Added comments
    I added comments while trying to understand this code so thought might as well put it here.

--- prototype/current_sensor_test/current_sensor_test.ino
+++ prototype/current_sensor_test/current_sensor_test.ino
@@ -1,57 +1,67 @@
-#include <Wire.h>
-#include <Adafruit_INA219.h>
+#include <Wire.h> // Include the Wire library for I2C communication
+#include <Adafruit_INA219.h> // Include the Adafruit library to control INA219 sensor
 
-Adafruit_INA219 ina219;
-float totalPower = 0;
+Adafruit_INA219 ina219; // Create a new object ina219. This will be used to interact with the INA219 sensor
 
-void setup(void) 
+float totalPower = 0; // Declare a variable to store the total power consumption
+
+void setup(void) // Function to set up the initial state
 {
-  Serial.begin(115200);
-  while (!Serial) {
-      // will pause Zero, Leonardo, etc until serial console opens
-      delay(1);
-  }
-    
-  Serial.println("Hello!");
-  
-  // Initialize the INA219.
-  // By default the initialization will use the largest range (32V, 2A).  However
-  // you can call a setCalibration function to change this range (see comments).
-  if (! ina219.begin()) {
-    Serial.println("Failed to find INA219 chip");
-    while (1) { delay(10); }
-  }
-  // To use a slightly lower 32V, 1A range (higher precision on amps):
-  //ina219.setCalibration_32V_1A();
-  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
-  //ina219.setCalibration_16V_400mA();
+  Serial.begin(115200); // Start serial communication for debugging and output
 
-  Serial.println("Measuring voltage and current with INA219 ...");
+  // This loop will pause Arduino Zero, Leonardo, etc until serial console opens
+  while (!Serial) {
+    delay(1);
+  }
+
+  Serial.println("Hello!"); // Send "Hello!" to the serial monitor
+
+  // Initialize the INA219
+  if (! ina219.begin()) {
+    // If the INA219 sensor didn't initialize, print an error
+    Serial.println("Failed to find INA219 chip");
+
+    // Stop the code execution indefinitely until the chip is found
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("Measuring voltage and current with INA219 ..."); // Print a message to the monitor informing we are starting to measure
 }
 
-void loop(void) 
+void loop(void) // Function that loops forever and where the main code goes
 {
-  float shuntvoltage = 0;
-  float busvoltage = 0;
-  float current_mA = 0;
-  float loadvoltage = 0;
-  float power_mW = 0;
+  float shuntvoltage = 0; // Declare a variable to store the shunt voltage
+  float busvoltage = 0; // Declare a variable to store the bus voltage
+  float current_mA = 0; // Declare a variable to store the current in mA
+  float loadvoltage = 0; // Declare a variable to store the load voltage
+  float power_mW = 0; // Declare a variable to store the power in mW
 
+  // Call functions to obtain measures from the INA219 sensor
   shuntvoltage = ina219.getShuntVoltage_mV();
   busvoltage = ina219.getBusVoltage_V();
   current_mA = ina219.getCurrent_mA();
   power_mW = ina219.getPower_mW();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
-  power_mW = current_mA * loadvoltage;
-  totalPower += power_mW;
-  
-  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Current:       "); Serial.print(current_mA ); Serial.println(" mA");
-  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
-  Serial.print("Total Power: "); Serial.print(totalPower); Serial.println(" mW");
-  Serial.println("");
+  loadvoltage = busvoltage + (shuntvoltage / 1000); // Calculate the load voltage
+  power_mW = current_mA * loadvoltage; // Calculate the power
 
-  delay(1000);
+  totalPower += power_mW; // Update total power
+
+  // Print the values to the Serial Monitor
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); 
+  Serial.println(" V");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); 
+  Serial.println(" mV");
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); 
+  Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(current_mA); 
+  Serial.println(" mA");
+  Serial.print("Power:         "); Serial.print(power_mW); 
+  Serial.println(" mW");
+  Serial.print("Total Power:   "); Serial.print(totalPower); 
+  Serial.println(" mW");
+  Serial.println("");  
+
+  delay(1000);  // Pause for a second before the next measurement
 }
